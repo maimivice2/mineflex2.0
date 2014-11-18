4890dcc80c96d22789405d86eac1f1903d43a4c3
maimvice13 <dhooghe.glenn@hotmail.com>
2014-11-18 17:01:01 +0100

    get started

---
+++ src/main/java/com/maimivice2/mineflex/mineflex.java
@@ -0,0 +1,37 @@
+package com.maimivice2.mineflex;
+
+import com.oracle.jrockit.jfr.EventDefinition;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="mineflex", name="mineflex v1.0", version="1.7.10-1.0")
+public class mineflex
+{
+    Mod.Instance("mineflex")
+    public static mineflex instance;
+
+
+
+    @Mod.EventHandler
+    public void PreInit(FMLPreInitializationEvent event)
+    {
+
+
+    }
+
+    @Mod.EventHandler
+    public void Init (FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void PostInit (FMLPostInitializationEvent event)
+    {
+
+
+    }
+
+}
